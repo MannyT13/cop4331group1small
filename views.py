ab085a0e3acbbe927ab81a7e6c90273dcf9e9d1f
devints47 <devints47@gmail.com>
2018-06-04 01:14:30 -0400

    Hotfix -- missing parenthesis

--- views.py
+++ views.py
@@ -60,7 +60,7 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'logged_in_user' not in session:
-            return redirect(url_for('login')
+            return redirect(url_for('login'))
         return f(*args, **kwargs)
     return decorated_function
 
