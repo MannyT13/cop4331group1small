af1283ff8a60d759d919352f2ee57b4b4995cfd6
umadapuma <umadatt.tiwari@knights.ucf.edu>
2018-06-06 15:12:51 -0400

    took out ajax

--- static/js/main.js
+++ static/js/main.js
@@ -67,20 +67,26 @@ $(document).ready(function(){
             }
         });
     });
-
-
-    $("#search").on("keyup", function() {
-        var value = $(this).val().toLowerCase();
-        $.ajax({
-            type:"POST",
-            success: function(){
-                $("tr").filter(function() 
-                {
-                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-                
-        });
+    $('#search').keyup(function() {
+function myFunction(){
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      } 
     }
-      });
+}
     });
 
 
