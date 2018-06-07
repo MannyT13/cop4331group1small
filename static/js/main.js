b929aeacec91e3c9e33096d990497707ddd906c0
devints47 <devints47@gmail.com>
2018-06-06 21:29:34 -0400

    Fix for adding contact adding extra columns

--- static/js/main.js
+++ static/js/main.js
@@ -6,11 +6,10 @@ $(document).ready(function(){
             data: $('#add_contact_form').serialize(),
             success: function(data){
                 console.log(data);
-                var new_contact = '<tr><td>' + ($('tbody').children().length) + '</td><td>' + data.first_name + '</td><td>\
-                ' + data.last_name + '</td><td>' + data.address1 + '</td><td>' + data.address2 + '</td><td>\
-				' + data.email + '</td><td>' + data.phone + '</td><td>' + data.city + '</td><td>\
-				' + data.zip + '</td><td><button class="remove-button" type="button" class="btn btn-default btn-sm" data-button="\
-				' + data.id + '"><span class="glyphicon glyphicon-remove" href="#"></span> Remove </button></td></tr>';
+                var new_contact = '<tr><td>' + data.first_name + '</td><td>\
+                ' + data.last_name + '</td><td>\
+				' + data.phone + '</td><td>' + data.email + '</td><td><button type="button" class="btn btn-danger btn-sm remove-button" data-button="\
+				' + data.id + '"><span class="glyphicon glyphicon-remove" href="#"></span> Delete Contact </button></td></tr>';
                 $('tbody').append(new_contact);
             }
         });
