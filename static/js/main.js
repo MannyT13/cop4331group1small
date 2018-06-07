8f9279ac89b777566212d7bb29b9d40c7305ba31
devints47 <devints47@gmail.com>
2018-06-06 21:03:38 -0400

    Cleaned up unused functions and added an ajax call for updating a contact

--- static/js/main.js
+++ static/js/main.js
@@ -1,43 +1,4 @@
 $(document).ready(function(){    
-    //for the login error: if user has incorrect username of password
-    $('#loginForm').on('submit', function(event){
-        
-        //when user has a login error we will send the loginError.html  as a json and put in a div using ajax using
-        //return jsonify({'resp': render_template('loginError.html', loginError=True)})
-        //if the user logged in successfully
-        //return jsonify({'resp': 'loggedIn'})
-        //must 
-        $.ajax({
-            url:"/test",
-            type: "POST",
-            success: function(resp){
-                if(resp.resp != "loggedIn")
-                 $('div#alertDiv').html(resp.resp);
-            }
-        });
-    });
-
-
-    //for the registration error: if user has a already taken username and/or two different passwords
-    $('#registrationForm').on('submit', function(event){
-        
-        //when user has a registration error we will send the registrationError.html as a json and put in a div using ajax using
-        //return jsonify({'resp': render_template('registrationError.html', usernameTaken=True, passwordMatch=True)})
-        //make sure to put the right boolean values
-        //if the user registered  successfully
-        //return jsonify({'resp': 'registered'})
-
-        $.ajax({
-            url:"/test",
-            type: "POST",
-            success: function(resp){
-                if(resp.resp != "registered")
-                 $('div#alertDiv').html(resp.resp);
-            }
-        });
-    });
-
-
     $('#add_contact_submit').on('click', function(event){
         $.ajax({
             url:"/add_contact",
@@ -51,6 +12,23 @@ $(document).ready(function(){
 				' + data.zip + '</td><td><button class="remove-button" type="button" class="btn btn-default btn-sm" data-button="\
 				' + data.id + '"><span class="glyphicon glyphicon-remove" href="#"></span> Remove </button></td></tr>';
                 $('tbody').append(new_contact);
+            }
+        });
+    });
+
+    $('.update-contact-submit').on('click', function(event){
+        $.ajax({
+            url:"/update_contact",
+            type: "POST",
+            data: $(this).parent().serialize(),
+            success: function(data){
+                console.log('successful update')
+                // var new_contact = '<tr><td>' + ($('tbody').children().length) + '</td><td>' + data.first_name + '</td><td>\
+                // ' + data.last_name + '</td><td>' + data.address1 + '</td><td>' + data.address2 + '</td><td>\
+                // ' + data.email + '</td><td>' + data.phone + '</td><td>' + data.city + '</td><td>\
+                // ' + data.zip + '</td><td><button class="remove-button" type="button" class="btn btn-default btn-sm" data-button="\
+                // ' + data.id + '"><span class="glyphicon glyphicon-remove" href="#"></span> Remove </button></td></tr>';
+                // $('tbody').append(new_contact);
             }
         });
     });
